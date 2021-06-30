e04e20c8a57011e9e8c4f3a526f5888c9d0bfa3b
SyarifahIndah <syrfhindah85@gamil.com>
2021-06-30 18:34:52 +0700

    UserCrudProject2

---
+++ app/src/main/java/com/syarifah/usercrudproject2/model/User.java
@@ -0,0 +1,45 @@
+package com.syarifah.usercrudproject2.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+    @SerializedName("id")
+    private String id;
+
+    @SerializedName("Name")
+    private String name;
+
+    @SerializedName("Email")
+    private String email;
+
+    public User(){
+        public User(String id, String name, String email){
+            this.id = id;
+            this.name = name;
+            this.email = email;
+        }
+
+        public void setId(String id){
+            this.id=id;
+        }
+        public void setName(String name){
+            this.name=name;
+        }
+        public void setEmail(String email){
+            this.email=email;
+        }
+
+        public String getId(){
+            return id;
+        }
+
+        public String getName(){
+            return name;
+        }
+        public String getEmail(){
+            return email;
+        }
+    }
+
+
+}
